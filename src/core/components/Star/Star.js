726bc2cc2546445baf79444774157b8cb7638d79
Robert Jakubowicz <robert@robert-jakubowicz.com>
2019-07-23 22:01:40 -0400

    Fix: pass className prop to <Star />

--- src/core/components/Star/Star.js
+++ src/core/components/Star/Star.js
@@ -5,25 +5,31 @@ import PropTypes from "prop-types";
 const fontSize = '36px';
 
 
-const StarGlyth = ({onClick, color='black', children}) => (<div onClick={onClick} style={{fontSize, color}}>{children}</div>);
+const StarGlyth = ({onClick, color='black', className, children}) => (
+    <div onClick={onClick} style={{fontSize, color}} className={className}>
+        {children}
+    </div>
+);
+
 const StarGlythChecked = (props) => (<StarGlyth color='gold' {...props}>&#9733;</StarGlyth>);
 const StarGlythNormal = (props) => (<StarGlyth {...props}>&#9734;</StarGlyth>);
 
-const Star = ({onClick, checked=false}) => {
+const Star = ({onClick, checked=false, ...props}) => {
     if (checked) {
         return (
-            <StarGlythChecked onClick={onClick} />
+            <StarGlythChecked onClick={onClick} {...props} />
         );
     }
 
     return (
-        <StarGlythNormal onClick={onClick} />
+        <StarGlythNormal onClick={onClick} {...props}  />
     );
 };
 
 
 Star.propTypes = {
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    checked: PropTypes.bool
 };
 
 export default Star;
