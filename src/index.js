6f9db196c1dbdc2131a31cae49d3bccad81e9e2d
Robert Jakubowicz <robert@robert-jakubowicz.com>
2019-07-23 22:21:19 -0400

    Add: index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import * as serviceWorker from './serviceWorker';
+import {applyMiddleware, combineReducers, createStore} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import {reducerComics} from './comics/reducers';
+import {reducerFavorites} from './favorites/reducers';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import  thunk  from 'redux-thunk'
-import {reducerComics} from './comics/ducks';
 
 const middleware = [thunk];
-const store = createStore(reducerComics,  applyMiddleware(...middleware));
+const rootReducer = combineReducers({reducerComics, reducerFavorites});
+const store = createStore(
+    rootReducer,
+    {},
+    composeWithDevTools(applyMiddleware(...middleware))
+);
 
 ReactDOM.render(
-    <Provider store={store}><App /></Provider>, document.getElementById('root'));
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
